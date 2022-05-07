813cc91f3ccd35178b86e09f6788962d5da37c02
Arkenstone <trejo.allan@yahoo.com>
2022-05-07 09:23:43 -0700

    Commit Assignment 8.3

---
+++ week-8/trejo-calculator.py
@@ -0,0 +1,24 @@
+#----------------------
+# Title: trejo_calculator.py
+# Author: Professor Krasso
+# Date: 05 7 2022
+# Modified By: Allan Trejo
+# Description: Basic Calculator operations with Python
+#--------------------
+
+# adds 2 numbers and returns results
+def add(n1, n2):
+    return n1 + n2
+
+# subtacts 2 numbers and returns results
+def substract(n1, n2):
+    return n1 - n2
+
+# divides 2 numbers and returns results
+def divide(n1,n2):
+    return n1 / n2
+
+# Calls each functions and prints results to console
+print(add(10,5))
+print(substract(40,3))
+print(divide(3,6))
