97b88b1ea3f8606fe1c304a52a634bc86c7b6141
Arkenstone <trejo.allan@yahoo.com>
2022-05-07 09:12:27 -0700

    Added Comments

--- week-8/trejo_hello_world.py
+++ week-8/trejo_hello_world.py
@@ -1,1 +1,10 @@
+#----------------------
+# Title: trejo_hello_world.py
+# Author: Professor Krasso
+# Date: 05 7 2022
+# Modified By: Allan Trejo
+# Description: Intro to python
+#--------------------
+
+#Prints to console "Hello World"
 print('Hello World')
